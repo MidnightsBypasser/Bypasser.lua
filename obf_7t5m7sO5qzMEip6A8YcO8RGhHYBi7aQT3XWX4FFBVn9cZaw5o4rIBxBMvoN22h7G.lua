fc6de89e4ea5a7f3d7f7653b7731e72b623bff57
MidnightsBypasser <136227844+MidnightsBypasser@users.noreply.github.com>
2023-06-11 01:07:47 -0700

    Add files via upload

---
+++ obf_7t5m7sO5qzMEip6A8YcO8RGhHYBi7aQT3XWX4FFBVn9cZaw5o4rIBxBMvoN22h7G.lua
@@ -0,0 +1,192 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Midnight's Bypasser", "DarkTheme")
+local Tab = Window:NewTab("Homepage")
+local Section = Tab:NewSection("Manual Bypasser")
+Section:NewButton("Manual Bypasser", "ButtonInfo", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dwqdqdq/ChatBypasser/main/ManualBypasser?token=GHSAT0AAAAAACDTEZONZFSPV5YKTUOTU2CUZEFODKA"))()
+end)
+local Section = Tab:NewSection("Bypassed Words")
+Section:NewButton("Bypassed Word: ass", "ButtonInfo", function()
+local A_1 = "а⁥⁥⁥⁥ѕѕ"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+end)
+Section:NewButton("Bypassed Word: cum", "ButtonInfo", function()
+local A_3 = "с⁥⁥⁥⁥um"
+local A_4 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_3, A_4)
+end)
+Section:NewButton("Bypassed Word: cock", "ButtonInfo", function()
+local A_5 = "сосk"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: fuck", "ButtonInfo", function()
+local A_5 = "fu𐌑𝖔 ͜ ͣ c𐌑ḳ"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: fuck you", "ButtonInfo", function()
+local A_5 = "fu𐌑𝖔 ͜ ͣ c𐌑ḳ you"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: asshole", "ButtonInfo", function()
+local A_5 = "аѕѕһоlе"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: rape", "ButtonInfo", function()
+local A_5 = "rаре"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: Boobs", "ButtonInfo", function()
+local A_5 = "Вoobs"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: Titties", "ButtonInfo", function()
+local A_5 = "Тitties"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: sex", "ButtonInfo", function()
+local A_5 = "s⁥⁥⁥⁥⁥⁥ех"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: hoe", "ButtonInfo", function()
+local A_5 = "һое"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: porn", "ButtonInfo", function()
+local A_5 = "рогn⁥⁥⁥⁥⁥"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: nigga", "ButtonInfo", function()
+local A_5 = "п⁥iɡ⁥ɡ⁥а"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: penis", "ButtonInfo", function()
+local A_5 = "реп⁥is⁥⁥⁥⁥⁥"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: beaner", "ButtonInfo", function()
+local A_5 = "⁥bеаn⁥ег⁥⁥"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+local Tab = Window:NewTab("Sentences")
+local Section = Tab:NewSection("Bypassed Words - Sentences")
+Section:NewButton("Bypassed Word: #1 Fan of Titties", "ButtonInfo", function()
+local A_5 = "#1 Fan of Тitties"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: big black cock", "ButtonInfo", function()
+local A_5 = "⁥big black со⁥⁥⁥сk⁥⁥"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: Blowjob", "ButtonInfo", function()
+local A_5 = "⁥Вӏоwjob⁥⁥"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: Bites your cock", "ButtonInfo", function()
+local A_5 = "⁥Віtеѕ уоur сосk⁥⁥"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: cock sucker", "ButtonInfo", function()
+local A_5 = "⁥сосk suсkеr⁥⁥"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: fat ass hoe", "ButtonInfo", function()
+local A_5 = "f⁥аt а⁥ѕѕ һое⁥⁥"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+local Tab = Window:NewTab("Capitalized")
+local Section = Tab:NewSection("Bypassed Words - Capitalized")
+Section:NewButton("Bypassed Word: THOT", "ButtonInfo", function()
+local A_5 = "ТН⁥⁥⁥⁥⁥ОТ⁥⁥"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: XXX", "ButtonInfo", function()
+local A_5 = "ΧΧΧ⁥⁥"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: HENTAI", "ButtonInfo", function()
+local A_5 = "НЕΝТАӀ⁥⁥"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: BITCH", "ButtonInfo", function()
+local A_5 = "ВI⁥⁥⁥⁥⁥⁥TСН⁥⁥"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: BITCHES", "ButtonInfo", function()
+local A_5 = "ВIT⁥⁥⁥⁥⁥⁥⁥СНEЅ⁥⁥"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: SHIT", "ButtonInfo", function()
+local A_5 = "ЅНIТ⁥⁥"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: KYS", "ButtonInfo", function()
+local A_5 = "К⁥⁥⁥⁥⁥YЅ"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: BUTTHOLE", "ButtonInfo", function()
+local A_5 = "ВUТТ⁥⁥⁥⁥НOLЕ⁥⁥"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end)
+Section:NewButton("Bypassed Word: MASTURBATE", "ButtonInfo", function()
+local A_5 = "МАЅТURВАТЕ"
+local A_6 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_5, A_6)
+end) 
